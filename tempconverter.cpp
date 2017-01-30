19c32b6fde4af77c08fe1a6e2a64a4c739b9019c
ayoakinrinade <ayoakinrinade@gmail.com>
2017-01-29 19:25:45 -0800

    Created tempconvertor.cpp

---
+++ tempconverter.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int menuChoice;
+	double inTemp;
+	double outTemp;
+
+	cout << "CELSIUS AND FAHRENHEIT CONVERSION CALCULATOR\n";
+	cout << "============================================================\n";
+	cout << "(1) CELSIUS TO FAHRENHEIT (2) FAHRENHEIT TO CELSIUS (3) EXIT\n";
+	cout << "input: ";
+	cin >> menuChoice;
+
+	switch (menuChoice) {
+		case 1: {
+			cout << "\n\nTEMPERATURE IN CELSIUS: ";
+			cin >> inTemp;
+			outTemp = inTemp * 1.8 + 32;
+			cout << "TEMPERATURE IN FAHRENHEIT: " << outTemp;
+			return 0;
+		}
+		case 2: {
+			cout << "\n\nTEMPERATURE IN FAHRENHEIT: ";
+			cin >> inTemp;
+			outTemp = (inTemp - 32) * 5 / 9;
+			cout << "TEMPERATURE IN CELSIUS: " << outTemp;
+			return 0;
+		}
+		case 3: {
+			return 0;
+		}
+	}
+
+}
